af168664bf680b97d0ab99fdaaea042ff41dad24
Dowgojashan <134786599+Dowgojashan@users.noreply.github.com>
2024-01-04 18:04:53 +0800

    Update app_car_detector.py

--- app_car_detector.py
+++ app_car_detector.py
@@ -3,29 +3,33 @@ import numpy as np
 import cv2 as cv
 from flask import Flask, render_template, Response
 from camera_pi import Camera
-from picamera.array import PiRGBArray
 from picamera import PiCamera
+from picamera.array import PiRGBArray
 import basic.config as cfg
 import basic.visualizer as vis
 from basic import imgproc, helper
 from basic.logger import Logger
 from basic.cardetector import cardetector
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/statics')
+app.config['SERVER_NAME'] = '192.168.0.102:5000'
 
 LOG = Logger('app_car_detector')
 
 # 初始化 PiCamera
 camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+camera.resolution = (320, 240)
+camera.framerate = 1
+
+# 關閉 GPU 加速預覽
+camera.start_preview(fullscreen=False, window=(100, 100, 640, 480))
 
 # 等待相機預熱
 time.sleep(0.1)
 
 # 初始化車輛偵測器
 car_detector = cardetector()
+
 
 def gen():
     """Video streaming generator function."""
@@ -46,6 +50,7 @@ def gen():
             # 修改類別名稱為 'car'
             image = vis.plotBBoxes(image, bboxes, len(bboxes) * ['car'], scores)
 
+        rawCapture.truncate(0)
         # 回傳 JPEG 影像串流
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
@@ -60,11 +65,12 @@ def video_feed():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 def main():
     LOG.info('Raspberry Pi: Car Detection and PiCamera Streaming')
-
     # 開啟 Flask 伺服器，同時執行 PiCamera 影像串流
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
 
 if __name__ == '__main__':
     main()
+
